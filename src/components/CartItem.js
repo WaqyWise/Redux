cf2b47f5cc3e08dce21d46dd464b4fadad7d0629
WaqyWise <vakulenko.a@ukr.net>
2023-05-25 19:03:33 +0300

    Amount

---
+++ src/components/CartItem.js
@@ -0,0 +1,11 @@
+import { ChevronDown, ChevtonUp } from "../icons"
+
+const CartItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CartItem
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice  } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+const initialState = {
+    carItems : cartItems,
+    amount: 4,
+    total: 0,
+    isLoading: true,
+};
+
+const cartSlice = createSlice({
+    name:'cart',
+    initialState
+});
+
+export default cartSlice.reducer;
